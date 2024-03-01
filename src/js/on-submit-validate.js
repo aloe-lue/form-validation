0aca89c9a7c2470a306d625325f3ca716f18fe01
aloe <120464393+FireDragonGod@users.noreply.github.com>
2024-03-02 06:26:55 +0800

    Report validity from the top to bottom on submit

--- src/js/on-submit-validate.js
+++ src/js/on-submit-validate.js
@@ -1,5 +1,3 @@
-import zipCodes from './zip-codes';
-
 const onSubmitValidate = ({
   button,
   email,
@@ -15,33 +13,39 @@ const onSubmitValidate = ({
   const userPassword = document.querySelector(password);
   const userConfirmPassword = document.querySelector(confirmPassword);
 
-  // ? see to it that all of the value's are valid and filled properly
   const isEmailValid = () => {
     if (!emailInput.validity.valid) {
+      emailInput.reportValidity();
       return 1;
     }
     return 0;
   };
 
-  // if a user has already selected a country you want to return 1 so it is true;
   const didSelectACountry = () => {
     if (selectCountry.validity.valueMissing) {
+      selectCountry.reportValidity();
       return 1;
     }
     return 0;
   };
 
-  // you want to check if the zip code validity is valid otherwise
   const isZipCodeValid = () => {
     if (!zipCodeInput.validity.valid) {
+      zipCodeInput.reportValidity();
       return 1;
     }
     return 0;
   };
 
-  // you want to check if the user password is match with confirm password
   const isPasswordMatched = () => {
-    if (userPassword.value !== userConfirmPassword.value) {
+    if (
+      userPassword.value !== userConfirmPassword.value ||
+      // report validity even when their value is missing
+      (userConfirmPassword.validity.valueMissing &&
+        userPassword.validity.valueMissing)
+    ) {
+      userConfirmPassword.reportValidity();
+      userPassword.reportValidity();
       return 1;
     }
     return 0;
@@ -49,17 +53,12 @@ const onSubmitValidate = ({
 
   const getTotal = (array) => array.reduce((acc, currVal) => acc + currVal, 0);
 
-  // todo: report validity heirarchy if user did not enter a valid email => reportValidity()
-  /* * * * * */ // todo: next => select; next => zip code; next => passwords;
-
-  // you want user to prevent user from submitting the form to server when to submit cases are true;
   const dontSendToServer /** ayiie */ = (e) => {
     const toSubmit = [
-      isEmailValid(),
+      isPasswordMatched(),
+      isZipCodeValid(),
       didSelectACountry(),
       isEmailValid(),
-      isZipCodeValid(),
-      isPasswordMatched(),
     ];
 
     if (getTotal(toSubmit) > 0) {
@@ -69,11 +68,7 @@ const onSubmitValidate = ({
     return true;
   };
 
-  // ?  warn the user what they have left to do
-
-  // here are the things to keep in mind to allow the submission all of the inputs including select element should return 1 otherwise might prevent this form from submitting to server side as if i have a server side hehe
   submitBtn.addEventListener('click', (e) => {
-    // know that in JavaScript function can be values so iterate all of the funtions to know if all of them returns 1 then put them in the server
     dontSendToServer(e);
   });
 };
